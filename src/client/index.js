404581ffb76fa795577675f84b9700ec374a411f
Jennifer  Piper <jenniferpiper@Jennifers-MacBook-Pro.local>
2020-05-03 16:27:18 -0700

    install cors, body-parser

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+//this file should import the main function of your application javascript, 
+//it should import your scss, and it should export your main function 
+//from your application javascript. But in order to import, where will 
+//you need to export it?
+
+//import from client/js/app.js
+//import from client/styles/style.scss
+//export client/js/app.js main function
+//add export to style.scss and app.js
